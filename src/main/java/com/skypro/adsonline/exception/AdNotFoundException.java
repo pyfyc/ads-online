4c78a3f1fbbbcfcdf241915c130498c615bd546e
Tamerlan <ishmaevtamerlan@mail.ru>
2023-04-30 16:14:30 +0500

    Adding CommentEntity, CommentRepository, CommentMapper

--- src/main/java/com/skypro/adsonline/exception/AdNotFoundException.java
+++ src/main/java/com/skypro/adsonline/exception/AdNotFoundException.java
@@ -2,7 +2,6 @@ package com.skypro.adsonline.exception;
 
 public class AdNotFoundException extends RuntimeException {
 
-
     public AdNotFoundException(String message) {
         super(message);
     }
