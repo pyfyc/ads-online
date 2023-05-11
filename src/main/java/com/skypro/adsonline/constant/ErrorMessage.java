02f3e9ae57ff3abc18f564516df98d0ca5c70dd5
Olga <dochnohi@yandex.ru>
2023-05-12 00:35:19 +0300

    Added role and admin rights for comments for methods deleteComment and updateComment

--- src/main/java/com/skypro/adsonline/constant/ErrorMessage.java
+++ src/main/java/com/skypro/adsonline/constant/ErrorMessage.java
@@ -5,4 +5,5 @@ public class ErrorMessage {
     public static final String AD_NOT_FOUND_MSG = "Объявление не найдено (id = %s).";
     public static final String COMMENT_NOT_FOUND_MSG = "Коментарий не найден (id = %s).";
     public static final String WRONG_PASS_MSG = "Введен неправильный пароль (пользователь %s).";
+    public static final String ACCESS_DENIED_MSG = " Нет прав доступа (пользователь %s). ";
 }
